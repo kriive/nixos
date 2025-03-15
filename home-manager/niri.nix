a1b0ddc548171f006129fb0e7346380517d3a9e3
Manuel Romei <manuel.romei@outlook.it>
2025-03-15 14:57:10 +0100

    feat: add niri configuration

--- home-manager/niri.nix
+++ home-manager/niri.nix
@@ -1,21 +1,170 @@
-{ ... }:
+{ pkgs, config, ... }:
 
 {
-  niri = {
+  home.packages = with pkgs; [
+    xwayland-satellite
+  ];
+
+  programs.niri = {
     settings = {
-      binds = {
-        "Mod+Return".action.spawn = "foot";
-        "Mod+space".action.spawn = 'tofi-drun "--drun-launch=true"';
-        "Mod+Q".action.close-window = true;
-        "Mod+Left" = { focus-column-left };
-        "Mod+Down" = { focus-window-down };
-        "Mod+Up"   = { focus-window-up };
-        "Mod+Right"= { focus-column-right };
-        "Mod+H"    ={ focus-column-left };
-        "Mod+J"    ={ focus-window-down };
-        "Mod+K"    ={ focus-window-up };
-        "Mod+L"    ={ focus-column-right };
+      prefer-no-csd = true;
+      outputs = {
+        "Samsung Electric Company U28E570 HTPKA02864" = {
+          scale = 2;
+        };
+
+        "*" = {
+          background-color = "#111416";
+        };
       };
+
+      environment = {
+        CLUTTER_BACKEND = "wayland";
+        DISPLAY = ":0";
+        GDK_BACKEND = "wayland";
+        MOZ_ENABLE_WAYLAND = "1";
+        NIXOS_OZONE_WL = "1";
+        QT_QPA_PLATFORM = "wayland";
+        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+        _JAVA_AWT_WM_NONREPARENTING = "1";
+        SDL_VIDEODRIVER = "wayland";
+      };
+
+      spawn-at-startup = [
+        { command = [ "xwayland-satellite" ]; }
+        { command = [ "waybar" ]; }
+        {
+          command = [
+            "foot"
+            "--server"
+          ];
+        }
+      ];
+
+      input = {
+        touchpad = {
+          click-method = "clickfinger";
+          dwt = true;
+          dwtp = true;
+          natural-scroll = true;
+          scroll-method = "two-finger";
+          tap = true;
+          tap-button-map = "left-right-middle";
+          accel-profile = "adaptive";
+        };
+        focus-follows-mouse.enable = true;
+        warp-mouse-to-focus = true;
+        workspace-auto-back-and-forth = true;
+        keyboard.xkb.layout = "us";
+        keyboard.xkb.variant = "altgr-intl";
+        keyboard.xkb.options = "compose:menu,level3:ralt_switch";
+      };
+
+      binds =
+        with config.lib.niri.actions;
+        let
+          sh = spawn "bash" "-c";
+        in
+        {
+          "Mod+Return".action.spawn = "footclient";
+          "Mod+space".action = sh ''tofi-drun | xargs niri msg action spawn --'';
+          "Mod+Shift+q".action = close-window;
+          "Mod+Left".action = focus-column-left;
+          "Mod+Down".action = focus-window-down;
+          "Mod+Up".action = focus-window-up;
+          "Mod+Right".action = focus-column-right;
+          "Mod+h".action = focus-column-left;
+          "Mod+j".action = focus-window-down;
+          "Mod+k".action = focus-window-up;
+          "Mod+l".action = focus-column-right;
+          "Mod+Shift+Left".action = move-column-left;
+          "Mod+Shift+Down".action = move-window-down;
+          "Mod+Shift+Up".action = move-window-up;
+          "Mod+Shift+Right".action = move-column-right;
+          "Mod+Shift+H".action = move-column-left;
+          "Mod+Shift+J".action = move-window-down;
+          "Mod+Shift+K".action = move-window-up;
+          "Mod+Shift+L".action = move-column-right;
+          "Mod+Home".action = focus-column-first;
+          "Mod+End".action = focus-column-last;
+          "Mod+Ctrl+Home".action = move-column-to-first;
+          "Mod+Ctrl+End".action = move-column-to-last;
+          # "Mod+Ctrl+Left".action = focus-monitor-left;
+          # "Mod+Ctrl+Down".action = focus-monitor-down;
+          # "Mod+Ctrl+Up".action = focus-monitor-up;
+          # "Mod+Ctrl+Right".action = focus-monitor-right;
+          "Mod+Shift+WheelScrollDown".action = focus-column-right;
+          "Mod+Shift+WheelScrollUp".action = focus-column-left;
+          "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
+          "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
+          "Mod+Ctrl+H".action = focus-monitor-left;
+          "Mod+Ctrl+J".action = focus-monitor-down;
+          "Mod+Ctrl+K".action = focus-monitor-up;
+          "Mod+Ctrl+L".action = focus-monitor-right;
+
+          "Mod+Shift+Ctrl+Left".action = move-column-to-monitor-left;
+          "Mod+Shift+Ctrl+Down".action = move-column-to-monitor-down;
+          "Mod+Shift+Ctrl+Up".action = move-column-to-monitor-up;
+          "Mod+Shift+Ctrl+Right".action = move-column-to-monitor-right;
+          "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
+          "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
+          "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
+          "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
+
+          "Mod+Page_Down".action = focus-workspace-down;
+          "Mod+Page_Up".action = focus-workspace-up;
+          "Mod+U".action = focus-workspace-down;
+          "Mod+I".action = focus-workspace-up;
+          "Mod+Shift+Page_Down".action = move-column-to-workspace-down;
+          "Mod+Shift+Page_Up".action = move-column-to-workspace-up;
+          "Mod+Shift+U".action = move-column-to-workspace-down;
+          "Mod+Shift+I".action = move-column-to-workspace-up;
+
+          # "Mod+Shift+Page_Down".action = move-workspace-down;
+          # "Mod+Shift+Page_Up".action = move-workspace-up;
+          # "Mod+Shift+U".action = move-workspace-down;
+          # "Mod+Shift+I".action = move-workspace-up;
+
+          "Mod+1".action.focus-workspace = 1;
+          "Mod+2".action.focus-workspace = 2;
+          "Mod+3".action.focus-workspace = 3;
+          "Mod+4".action.focus-workspace = 4;
+          "Mod+5".action.focus-workspace = 5;
+          "Mod+6".action.focus-workspace = 6;
+          "Mod+7".action.focus-workspace = 7;
+          "Mod+8".action.focus-workspace = 8;
+          "Mod+9".action.focus-workspace = 9;
+
+          "Mod+Shift+1".action.move-column-to-workspace = 1;
+          "Mod+Shift+2".action.move-column-to-workspace = 2;
+          "Mod+Shift+3".action.move-column-to-workspace = 3;
+          "Mod+Shift+4".action.move-column-to-workspace = 4;
+          "Mod+Shift+5".action.move-column-to-workspace = 5;
+          "Mod+Shift+6".action.move-column-to-workspace = 6;
+          "Mod+Shift+7".action.move-column-to-workspace = 7;
+          "Mod+Shift+8".action.move-column-to-workspace = 8;
+          "Mod+Shift+9".action.move-column-to-workspace = 9;
+
+          "Mod+Comma".action = consume-window-into-column;
+          "Mod+Period".action = expel-window-from-column;
+
+          "Mod+R".action = switch-preset-column-width;
+          "Mod+F".action = maximize-column;
+          "Mod+Shift+F".action = fullscreen-window;
+          "Mod+C".action = center-column;
+
+          "Mod+Minus".action.set-column-width = "-10%";
+          "Mod+Equal".action.set-column-width = "+10%";
+
+          "Mod+Shift+Minus".action.set-window-height = "-10%";
+          "Mod+Shift+Equal".action.set-window-height = "+10%";
+          "Print".action = screenshot;
+          # "Ctrl+Print".action = screenshot-screen;
+          "Alt+Print".action = screenshot-window;
+          "Mod+Shift+E".action = quit;
+          "Mod+Shift+P".action = power-off-monitors;
+          "Mod+Shift+Ctrl+T".action = toggle-debug-tint;
+        };
     };
     package = pkgs.niri-unstable;
   };
